796f1dcebd1825580dc3638dc481a89eca22a7e5
Davide Gessa <gessadavide@gmail.com>
2017-05-12 11:42:47 +0200

    frontend

---
+++ docs/app.js
@@ -0,0 +1,23 @@
+var app = angular.module('delegateApp', []);
+
+app.controller('indexCtrl', function($scope, $http) {
+    $scope.accounts = [];
+    $scope.lastpayout = 0;
+    $scope.nextpayout = 0;
+
+    $http.get ('poollogs.json').then (function (res) {
+        $scope.lastpayout = res.data.lastpayout * 1000;
+        $scope.nextpayout = moment ($scope.lastpayout).add (1, 'week').valueOf();
+        $scope.accounts = [];
+
+        for (addr in res.data.accounts) {
+            var it = res.data.accounts[addr];
+            it['address'] = addr;
+            $scope.accounts.push (it);
+        }
+    });
+
+    $http.get ('https://wallet.lisknode.io/api/delegates/get?username=dakk').then (function (res) {
+        $scope.delegate = res.data.delegate;
+    });
+});
